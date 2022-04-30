7e3fbdbb9a32e84715fa628e194be5bca800010a
tim <zy751724547zy@gmail.com>
2022-04-30 11:46:01 +0800

    commit & push

--- src/hello.java
+++ src/hello.java
@@ -2,10 +2,8 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hello world!");
         System.out.println("你好 世界！");
+        System.out.println("测试 commit&push");
     }
-
-
-
 }
 
 
